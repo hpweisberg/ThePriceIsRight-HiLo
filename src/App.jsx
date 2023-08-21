e3a70ccf14db77328d594013b35de3b6d0a3cb85
hpweisberg <hpweisberg@gmail.com>
2023-08-21 11:51:08 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GameContainer from "./Components/GameContainer";
+
+function App() {
+
+  return (
+    <main className="app">
+      <h1 className="text-center">Hi Lo</h1>
+      <GameContainer />
+
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/GameContainer.jsx
@@ -0,0 +1,15 @@
+const GameContainer = () => {
+  return (
+    <main>
+      <div>
+        <p>Display selected pieces here</p>
+        <p>Display un-selected pieces here</p>
+      </div>
+      <div>
+        <p>Display game pieces here</p>
+      </div>
+    </main>
+  );
+}
+
+export default GameContainer;
